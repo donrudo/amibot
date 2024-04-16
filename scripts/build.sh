5a083a150faade1cd6f35211fa389deed4f62d8d
Don Rudo <pro@donrudo.com>
2024-04-15 21:35:23 -0700

    * fixes to the build.sh and start.sh to assume the use of venv

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+if [ -d "venv" ]; then
+    source venv/bin/activate
+else
+    python3 -m venv venv
+    source venv/bin/activate
+fi
+
 pip install build
+python -m build
 pip install -r requirements.txt
-pip -m build
 pip install -e .
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+source venv/bin/activate
 python3 amibot -c configs/amibot.conf
