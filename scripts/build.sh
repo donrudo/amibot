9cb24195664b3464f14089e92cbd17346a488965
Don Rudo <pro@donrudo.com>
2024-05-16 10:00:29 -0700

    - Removed python -m build call.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 if [ -d "venv" ]; then
-    source venv/bin/activate
+    source ./venv/bin/activate
 else
     python3 -m venv venv
-    source venv/bin/activate
+    source ./venv/bin/activate
 fi
 
-pip install build
-python -m build
-pip install -r requirements.txt
+python3 -m pip install build
+pip install -r ./requirements.txt
 pip install -e .
